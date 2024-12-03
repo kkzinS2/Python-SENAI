60abf105c292c6be25cda4e2982adb763fd27b55
kkzinS2 <kauaalmeida2004@gmail.com>
2024-12-03 11:27:20 -0300

    Aula 02

---
+++ Aula02.py
@@ -0,0 +1,9 @@
+print("Eu queria saber, tipo de você.... tipo assim, pô, tamo competindo, tamo competindo, gente")
+
+nome_user = "Kauã Pereira"    # tipo -> str = string
+idade_user = 20     # tipo -> int = inteiro
+altura_user = 1.91    # tipo -> float = quebrado
+maior_idade = True     # tipo -> bool = boolean
+
+print("Seja bem-vindo,", nome_user, "sua idade é", idade_user, "anos,", "você tem", altura_user,"metros.")
+print("Você é maior de idade?", maior_idade)
